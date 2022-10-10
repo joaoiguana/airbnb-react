ab8209e7403028ecf648a33e1b73bc56df537bfb
joao garcia <jiboiadamor@gmail.com>
2022-10-10 17:26:35 +0100

    failed to show marker on mapbox

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
 import ReactMapboxGl from 'react-mapbox-gl';
-import Flat from '../Flat'
-import FlatMarker from '../FlatMarker'
+import "mapbox-gl/dist/mapbox-gl.css";
+import Flat from '../Flat';
+import FlatMarker from '../FlatMarker';
 
 const Map = ReactMapboxGl({accessToken: 'pk.eyJ1Ijoiam9hb2lndWFuYSIsImEiOiJjbDZwMnFyM3kwZ2k1M2pxb3R5eWlvZ3FwIn0.SgcIRipy3dK0yZBLeaSf8Q'})
 
@@ -24,7 +25,7 @@ const App = () => {
         <input className='search' />
         <div className='flats'>
           {flats.map((flat) => {
-            return <Flat key={flat.id} imageUrl={flat.imageUrl} price={flat.price} name={flat.name} />;
+            return <Flat key={flat.id} imageUrl={flat.imageUrl} price={flat.price} name={flat.name} />
           })}
         </div>
       </div>
@@ -35,11 +36,15 @@ const App = () => {
           containerStyle={{ height: '100vh', width: '100%' }}
           style="mapbox://styles/mapbox/streets-v8"
         >
-          <FlatMarker price={150} lat={48.8566} lng={2.3522} />
+          {flats.map((flat) => {
+            return (
+              <FlatMarker key={flat.id} price={flat.price} lat={flat.lat} lng={flat.lng} />
+            );
+          })}
         </Map>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/FlatMarker/index.jsx
+++ src/components/FlatMarker/index.jsx
@@ -1,10 +1,13 @@
 import { Marker } from 'react-mapbox-gl';
+import './FlatMarker.scss';
 
 const FlatMarker = ({ lat, lng, price }) => {
   return (
-    <Marker coordinates={[lng, lat]} anchor="bottom">
-      <strong className="marker">{price}</strong>
-    </Marker>
+    <div className='marker'>
+      <Marker coordinates={[lng, lat]}>
+        <strong className="marker">{price}</strong>
+      </Marker>
+    </div>
   );
 };
 
