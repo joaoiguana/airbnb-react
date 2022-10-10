f7af8a65e5f3958f83dcf9153f4fb5d667d07e42
joao garcia <jiboiadamor@gmail.com>
2022-10-10 16:33:34 +0100

    added a map to the app

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
+import ReactMapboxGl from 'react-mapbox-gl';
 import Flat from '../Flat'
+
+const Map = ReactMapboxGl({accessToken: 'pk.eyJ1Ijoiam9hb2lndWFuYSIsImEiOiJjbDZwMnFyM3kwZ2k1M2pxb3R5eWlvZ3FwIn0.SgcIRipy3dK0yZBLeaSf8Q'})
 
 const API_URL = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json';
 
@@ -20,11 +23,17 @@ const App = () => {
         <input className='search' />
         <div className='flats'>
           {flats.map((flat) => {
-            return <Flat imageUrl={flat.imageUrl} price={flat.price} name={flat.name} />;
+            return <Flat key={flat.id} imageUrl={flat.imageUrl} price={flat.price} name={flat.name} />;
           })}
         </div>
       </div>
-      <div className='map'></div>
+      <div className='map'>
+        <Map
+          zoom={[14]}
+          center={[2.3522, 48.8566]}
+          containerStyle={{ height: '100vh', width: '100%' }}
+          style="mapbox://styles/mapbox/streets-v8" />
+      </div>
     </div>
   );
 }
