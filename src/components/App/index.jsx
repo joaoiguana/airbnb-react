4c6bf1f74dc106d3e870fa483abdbbdf36bed243
joao garcia <jiboiadamor@gmail.com>
2022-10-11 11:18:26 +0100

    working on search component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
 import ReactMapboxGl from 'react-mapbox-gl';
-import "mapbox-gl/dist/mapbox-gl.css";
+
 import Flat from '../Flat';
 import FlatMarker from '../FlatMarker';
+import Search from '../Search'
 
 const Map = ReactMapboxGl({accessToken: 'pk.eyJ1Ijoiam9hb2lndWFuYSIsImEiOiJjbDZwMnFyM3kwZ2k1M2pxb3R5eWlvZ3FwIn0.SgcIRipy3dK0yZBLeaSf8Q'})
 
@@ -11,6 +12,7 @@ const API_URL = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/mas
 
 const App = () => {
   const [flats, setFlats] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     fetch(API_URL)
@@ -18,11 +20,15 @@ const App = () => {
       .then(data => setFlats(data));
   }, []);
 
+  const handleSearch = (text) => {
+    setSearchText(text);
+  };
+
 
   return (
     <div className='app'>
       <div className='main'>
-        <input className='search' />
+        <Search onSearch={handleSearch} />
         <div className='flats'>
           {flats.map((flat) => {
             return <Flat key={flat.id} imageUrl={flat.imageUrl} price={flat.price} name={flat.name} />
@@ -32,14 +38,12 @@ const App = () => {
       <div className='map'>
         <Map
           zoom={[14]}
-          center={[2.3522, 48.8566]}
+          center={[2.34689, 48.884211]}
           containerStyle={{ height: '100vh', width: '100%' }}
           style="mapbox://styles/mapbox/streets-v8"
         >
           {flats.map((flat) => {
-            return (
-              <FlatMarker key={flat.id} price={flat.price} lat={flat.lat} lng={flat.lng} />
-            );
+            return <FlatMarker key={flat.id} price={flat.price} lat={flat.lat} lng={flat.lng} />
           })}
         </Map>
       </div>
--- src/components/FlatMarker/index.jsx
+++ src/components/FlatMarker/index.jsx
@@ -1,13 +1,12 @@
 import { Marker } from 'react-mapbox-gl';
 import './FlatMarker.scss';
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const FlatMarker = ({ lat, lng, price }) => {
   return (
-    <div className='marker'>
-      <Marker coordinates={[lng, lat]}>
-        <strong className="marker">{price}</strong>
-      </Marker>
-    </div>
+    <Marker coordinates={[lng, lat]} anchor="top">
+      <h1><strong className="marker">{price}</strong></h1>
+    </Marker>
   );
 };
 
